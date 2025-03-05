bca2dbe457092469baed512e2bb07f20b25287c4
HenryLee <lee@HenryLeedeMacBook-Pro.local>
2025-03-05 14:15:14 +0800

    .

--- src/mro.py
+++ src/mro.py
@@ -1,21 +1,35 @@
 class A:
+    """
+    A class
+    """
+
     def __init__(self):
         print("A init")
 
 
 class B(A):
+    """
+    B class
+    """
+
     def __init__(self):
         super().__init__()
         print("B init")
 
 
 class C(A):
+    """C class"""
+
     def __init__(self):
         super().__init__()
         print("C init")
 
 
 class D(B, C):
+    """
+    D class
+    """
+
     def __init__(self):
         print("D init")
         super().__init__()
